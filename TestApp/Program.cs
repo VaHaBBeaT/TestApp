d3e89cb08a40a4b9ef78c14408c72be0fd95e3c9
VaHaBBeaT <VaHaBBeaT@TAI-CHI>
2016-05-29 22:55:12 +0600

    Добавьте файлы проекта.

---
+++ TestApp/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Barcode { get; set; }
+
+        public Product (string name, int price, string barcode)
+        {
+            Name = name;
+            Price = price;
+            Barcode = barcode;
+        }
+    }
+    class Book:Product
+    {
+        public int bookPages { get; set; }
+        public int minAge { get; set; }
+        public string progLang { get; set; }
+        public string mainIngr { get; set; }
+        public string EsotericBook
+        {
+            get
+            {                
+                string esobook = Name + "\t";
+                return esobook;
+            }
+            
+
+        }
+
+        public void ProgrammingBook(int bookPages, string progLang)
+        {
+
+        }
+
+        public void CookBook(int bookPages, string mainIngr)
+        {
+
+        }
+        public Book (string name, int price, string barcode, int age, string lang, string ingr) : base(name, price, barcode)
+        {
+            minAge = age;
+            progLang = lang;
+            mainIngr = ingr;
+        }
+    }
+
+    class Disc
+    {
+        
+        public static void CD()
+        {
+
+        }
+
+        public static void DVD()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=====Книги=====");
+
+            Console.WriteLine("=====Диски=====");
+
+            Console.ReadLine();
+        }
+    }
+}
